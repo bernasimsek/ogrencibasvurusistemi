8641b33d38e7d84c77d2a1832cc23e82a8970b18
berna <simskberna@gmail.com>
2021-10-17 17:29:31 +0300

    all files added

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Yaz Okulu Başvurusu', url: '/folder/YazOkuluBasvuru', icon: 'create' },
+    { title: 'Yatay Geçiş Başvurusu', url: '/folder/YatayGecisBasvuru', icon: 'create' },
+    { title: 'DGS Başvurusu', url: '/folder/DGSBasvuru', icon: 'create' },
+    { title: 'ÇAP Başvurusu', url: '/folder/CAPBasvuru', icon: 'create' },
+    { title: 'Ders İntibakı Başvurusu', url: '/folder/DersIntibaki', icon: 'create' },
+    
+  ];
+  public labels = ['Başvurusu Bitenler', 'Başvurusu Devam Edenler'];
+  constructor() {}
+}
