c8d316dbe38f47c86944e957a6bdac989a561b89
Berna Simsek <49639453+simskberna@users.noreply.github.com>
2021-11-05 14:25:47 +0300

    Update yaz-okulu-basvuru.page.ts

--- src/app/yaz-okulu-basvuru/yaz-okulu-basvuru.page.ts
+++ src/app/yaz-okulu-basvuru/yaz-okulu-basvuru.page.ts
@@ -18,6 +18,7 @@ import {
   Router
 } from '@angular/router';
 import {
+  AlertController,
   LoadingController
 } from '@ionic/angular';
 import firebase from 'firebase/compat/app';
@@ -27,34 +28,81 @@ import {
 import {
   AuthService
 } from 'src/app/services/auth.service';
+import { map } from 'rxjs/operators';
+
+export interface Fakulteler {
+  fakulteAd: any;
+  bolumler: any;
+ 
+ }
 @Component({
   selector: 'app-yaz-okulu-basvuru',
   templateUrl: './yaz-okulu-basvuru.page.html',
   styleUrls: ['./yaz-okulu-basvuru.page.scss'],
 })
 export class YazOkuluBasvuruPage implements OnInit {
+  //akulteler$: Observable<Fakulteler[]>;
+  fakulteler: Observable<Fakulteler[]>;
+  fakulteAd:any;
+  bolumAd:any;
+  ad:any;
+  soyad:any;
+  no:any;
 
-  fakulteler: any;
-  fakulteAd : any;
-  constructor(private database: AngularFirestore, private authservice: AuthService, private storage: AngularFireStorage,
+  fakulteCollection: AngularFirestoreCollection<Fakulteler>;
+  constructor(public loadingCtrl: LoadingController, private alertCtrl: AlertController,public afAuth: AngularFireAuth, private database: AngularFirestore, private authservice: AuthService, private storage: AngularFireStorage,
     private router: Router) {
-      this.fetchAllData();
+
+    this.afAuth.signInAnonymously();
+    this.fakulteCollection = this.database.collection('fakulteler'); 
+    this.fakulteler = this.fakulteCollection.valueChanges();
+ 
+      /*this.fakulteler$ = this.database.collection<Fakulteler>('fakulteler')
+      .snapshotChanges().pipe(
+        map(actions => actions.map(a => {
+          const data = a.payload.doc.data() as Fakulteler;
+          const id = a.payload.doc.id;
+          return {...data};
+        }))
+      );*/
    }
   
   ngOnInit() {
-      
+    //this.fakulteler$.subscribe(data => console.log(Array.from(data.values())[0]));
   }
- 
-   fetchAllData() {
-    firebase.database().ref('fakulteler').once('value',function(snapshot){
-      snapshot.forEach(
-        function(ChildSnapshot) {
-          let fakulteAd = ChildSnapshot.val().fakulteAd;
-        }
-      )
+   add(){
+    this.showalert();
+     try{
+      this.database.collection("basvurular").add({
+        'fakulteAd':this.fakulteAd,
+        'bolumAd':this.bolumAd,
+        'ad':this.ad,
+        'soyad':this.soyad,
+        'no':this.no,
+     })
+     this.uploadAppl();
+    }
+     catch(err){
+        console.log(err);
+     }
+    
+   
+   
+   }async showalert() {
+    var load = await this.alertCtrl.create({
+      message: 'Başvurunuz Yapıldı',
     })
+    load.present();
   }
 
+  async uploadAppl() {
+ 
+        this.database.doc(`user/${this.authservice.getUserUid()}`).update({
+          basvuru: 'yaz okulu basvurusu yapıldı'
+        })
+       
+     
+  }
 }
 
 
